c4a372fd8a8d9764b0781f6f85f550c89e357dd0
Olesya Vetoshkina <our_post-office@mail.ru>
2023-06-03 05:32:15 +0300

    third commit

--- e2e/validaty.test.js
+++ e2e/validaty.test.js
@@ -8,7 +8,7 @@ describe('Card Form', () => {
 
   beforeEach(async () => {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 100,
       devtools: true,
     });
